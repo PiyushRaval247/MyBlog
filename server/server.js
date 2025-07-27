22767e3b53de7cc1e916cc8994c2ad26fa7490fa
PiyushRaval247 <piyushraval2474@gmail.com>
2025-07-27 12:11:02 +0530

    cors edited

--- server/server.js
+++ server/server.js
@@ -13,7 +13,16 @@ await connectDB();
 
 
 //Middleware
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "https://my-blog-piyush.vercel.app/",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 
